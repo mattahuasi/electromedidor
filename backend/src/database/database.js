351fd74f0ebaa62cd654d137f05890db04c30035
mattahuasi <mattahuasi@gmail.com>
2024-01-09 01:01:49 -0400

    Creating admin

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,5 +1,15 @@
 import Sequelize from "sequelize";
 import "dotenv/config";
 
-export const sequelize = new Sequelize(process.env.DB_URL);
+// export const sequelize = new Sequelize(process.env.DB_URL);
+
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true, // Opciones de SSL específicas de Sequelize
+      rejectUnauthorized: false, // Podría ser necesario en algunos casos, depende de la configuración de Render
+    },
+  },
+});
+
 sequelize.options.logging = false;
--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,31 @@ import "./models/Bill.js";
 import "./models/Report.js";
 import "dotenv/config";
 
+import { User, Employee } from "./models/User.js";
+import bcrypt from "bcryptjs";
+
 async function main() {
   try {
     await sequelize.sync({ alter: true });
     console.log("Connection has been established successfully.");
+    const user = await Employee.findOne({ include: { model: User } });
+    if (!user) {
+      const passwordHash = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
+      const newUser = await User.create({
+        firstName: process.env.ADMIN_FIRSTNAME,
+        lastName: process.env.ADMIN_LASTNAME,
+        ci: "10000000",
+        phone: "",
+        email: process.env.ADMIN_EMAIL,
+        password: passwordHash,
+      });
+      const newEmployee = await Employee.create({
+        staff: true,
+        admin: true,
+        userId: newUser.id,
+      });
+      console.log("Admin was created successfully");
+    }
     const server = createServer(app);
     webSocket(server);
     server.listen(app.get("port"), () => {
