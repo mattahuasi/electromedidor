025dd06bae4ad62d35b32f5bb58088441697274e
mattahuasi <mattahuasi@gmail.com>
2024-01-22 22:31:46 -0400

    Database deploy

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import "dotenv/config";
 
-export const sequelize = new Sequelize(
+/* export const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
@@ -11,5 +11,8 @@ export const sequelize = new Sequelize(
     dialect: process.env.DB_CONNECTION,
     timezone: process.env.DB_TIMEZONE,
   }
-);
+); */
+
+export const sequelize = new Sequelize(process.env.DB_URL);
+
 sequelize.options.logging = false;
