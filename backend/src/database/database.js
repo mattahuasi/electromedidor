1d2ba5316981ba927e6bcfeac28ee45bf7cabcf1
mattahuasi <mattahuasi@gmail.com>
2024-01-31 19:17:17 -0400

    Auth updated for deploy

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,17 +1,17 @@
 import Sequelize from "sequelize";
 import "dotenv/config";
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_CONNECTION,
-    timezone: process.env.DB_TIMEZONE,
-  }
-);
+// export const sequelize = new Sequelize(
+//   process.env.DB_DATABASE,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_CONNECTION,
+//     timezone: process.env.DB_TIMEZONE,
+//   }
+// );
 
 // export const sequelize = new Sequelize(process.env.DB_URL, {
 //   dialectOptions: {
@@ -22,6 +22,6 @@ export const sequelize = new Sequelize(
 //   },
 // });
 
-// export const sequelize = new Sequelize(process.env.DB_URL);
+export const sequelize = new Sequelize(process.env.DB_URL);
 
 sequelize.options.logging = false;
